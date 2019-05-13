4712e3ea90dbe6f559245f216d11b6cbcd9de87f
chencar <carl.chen@hp.com>
2019-05-13 13:40:02 +0800

    commit

---
+++ Framework_Kernel/ConfigurationEngine.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# @Time    : 5/13/2019 1:27 PM
+# @Author  : Carl
+# @Email   : carl.chen@hp.com
+# @File    : ConfigurationEngine.py
+# @Project : Automation-Framework
+class ConfigurationEngine:
+    def __init__(self,status='off'):
+        self.status=status
+    def start(self):
+        print('start')
+    def stop(self):
+        print('stop')
+
+def execute():
+    pass
