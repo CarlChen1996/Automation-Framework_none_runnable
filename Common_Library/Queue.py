5b5456b4a95442618b4edafa499b5e51d97c31bf
Balance Cheng <balance.cheng@hp.com>
2019-05-13 14:14:54 +0800

    Add files via upload

--- Common_Library/Queue.py
+++ Common_Library/Queue.py
@@ -1,20 +1,53 @@
+# -*- coding: utf-8 -*-
+# @Time    : 5/13/2019 2:06 PM
+# @Author  : Balance
+# @Email   : balance.cheng@hp.com
+# @File    : Queue.py
+# @Project : framework
+
 
 
 class Queue:
     def __init__(self):
-        self._task_list = []
+        self.task_list = []
 
     def insert_task(self, index=-1, task=''):
-        self._task_list.insert(index, task)
+        self.task_list.insert(index, task)
 
     def remove_task(self, task):
-        self._task_list.remove(task)
+        self.task_list.remove(task)
 
     def set_order(self, index, task):
         pass
 
     def clear(self):
-        self._task_list = []
+        self.task_list = []
 
     def get_task_list(self):
-        return self._task_list
+        return self.task_list
+
+
+class AssembleQueue(Queue):
+    def __init__(self):
+        self.task_list = []
+
+    def build_task(self, task):
+        pass
+
+
+class ExecuteQueue(Queue):
+    def __init__(self):
+        self.task_list = []
+
+    def deploy(self, task, host):
+        task.deploy(host)
+        print('Q deploy task')
+
+    def execute(self, task):
+        print('Q execute task')
+
+    def check_status(self, task):
+        print('Q check status')
+
+    def collect_result(self, task):
+        print('Q collect result')
--- Common_Library/Task.py
+++ Common_Library/Task.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Time    : 5/13/2019 2:07 PM
+# @Author  : Balance
+# @Email   : balance.cheng@hp.com
+# @File    : Task.py
+# @Project : framework
 
 
 
@@ -12,15 +18,10 @@ class Task:
         self._state = ''
         self._need_build = need_build
 
+    # ------Below is set/get function for member-----------
     def insert_script(self, script):
         # set scripts
         self._script_list.append(script)
-
-    def get_script(self, name):
-        # get scripts
-        for script in self._script_list:
-            if script.name == name:
-                return script
 
     def get_script_list(self):
         # get scripts
@@ -29,17 +30,11 @@ class Task:
     def insert_exe_list(self, file):
         self._exe_list.append(file)
 
-    def get_exe(self, name):
-        pass
-
     def get_exe_list(self):
         return self._exe_list
 
     def insert_uut_list(self, uut):
         self._uut_list.append(uut)
-
-    def get_uut(self, name):
-        pass
 
     def get_uut_list(self):
         return self._uut_list
@@ -53,6 +48,45 @@ class Task:
     def get_status(self):
         return self._status
 
+    def set_status(self, status):
+        self._status == status
+
     def get_state(self):
         return self._state
+
+    def set_state(self, state):
+        self._state = state
+
+    # ---------Below is funtion for task-------------
+    def build(self, host):
+        host.build(self)
+
+    def get_scripts(self, host):
+        host.get_scripts(self)
+
+    def execute(self, host):
+        host.execute_task(self)
+
+    def deploy(self, host):
+        host.deploy(self)
+
+    def check_status(self, host):
+        host.check_status(self)
+
+    def collect_result(self, host):
+        host.collect_result(self)
+
+    def terminate(self, host):
+        pass
+
+    def get_exe_duration(self):
+        pass
+
+    def validate_host(self, validator):
+        for host in self._uut_list:
+            validator.validate(host)
+
+    def validate_scripts(self, validator):
+        for script in self._script_list:
+            validator.validate(script)
 
