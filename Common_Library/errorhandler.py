66ad4756ff50285745e116947afcd50e300d32b1
chencar <carl.chen@hp.com>
2019-05-13 14:00:10 +0800

    commit

---
+++ Common_Library/errorhandler.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# @Time    : 5/13/2019 1:51 PM
+# @Author  : Carl
+# @Email   : carl.chen@hp.com
+# @File    : errorhandler.py
+# @Project : Automation-Framework
+class ErrorHandler:
+    def __init__(self,error_code):
+        self.error_code=error_code
+
+    def handle(self,error_code):
+        pass
+    def exit(self):
+        pass
+    def reset_framework(self):
+        pass
+    def rest_engine(self,engine):
+        pass
+    def continue_work(self):
+        pass
