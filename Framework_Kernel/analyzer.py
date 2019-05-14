d7fc07444916911d051aab4081803adade58feea
bamboo.pan <bamboo.pan@hp.com>
2019-05-14 14:56:41 +0800

    add load data code

--- Framework_Kernel/analyzer.py
+++ Framework_Kernel/analyzer.py
@@ -4,15 +4,28 @@
 # @Email   : Bamboo.pan@hp.com
 # @File    : Analyzer.py
 # @Project : demo
-import sys
-
+import sys,os
+from Common_Library.file import YamlFile
 
 class Analyzer:
-    def __init__(self, file_list=None):
+    def __init__(self, file_list):
         self.file_list = file_list
 
     def load(self):
-        print(sys._getframe().f_code.co_name + "  finished")
+        res_tem_list=[]
+        for i in self.file_list:
+            f = YamlFile(os.path.dirname(i), os.path.basename(i))
+            file_handle=f.open()
+            res = f.read(file_handle)
+            res_tem_list.append(res)
+            print("load data {} finished".format(i))
+            f.close(file_handle)
+            print("close file {} finished".format(i))
+        print("load all data finised")
+        return res_tem_list
 
-    def generate(self):
-        print(sys._getframe().f_code.co_name + "  finished")
+    def generate(self,res_list):
+        print("generate data finished")
+        return res_list
+
+
