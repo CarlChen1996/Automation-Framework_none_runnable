11e4d04665db58b5da86b2f8f215cf4cd8b3c271
chengba <balance.cheng@hp.com>
2019-05-15 12:09:31 +0800

    123455

--- controller.py
+++ controller.py
@@ -9,13 +9,19 @@ print('====================================')
 task_list = []
 build_list = []
 deploy_list = []
-log.log('Begin to init configuration engine')
+
+log.log('start configuration engine')
 conf = configuration_engine.ConfigurationEngine()
 conf.start(build_list, deploy_list)
+log.log( "configurator  finished")
 print('====================================')
+log.log('start assemble engine')
 assemble = assemble_engine.AssembleEngine()
 assemble.tasklist = task_list
 assemble.start(build_list)
+log.log('assemble finished')
 print('======================================')
+log.log('start execution engine')
 exe = execution_engine.ExecutionEngine()
 exe.start(deploy_list, task_list)
+log.log('execution finished')
