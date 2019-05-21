546576cc12478572c963d7f21469e278b759fd2a
bamboo.pan <bamboo.pan@hp.com>
2019-05-21 16:47:09 +0800

    add control update

--- controller.py
+++ controller.py
@@ -27,79 +27,86 @@ def readinput(timeout):
     else:
         return None
 
-
 def operation():
-    global status_flag, assemble, exe, pipe, deploy_list, build_list, lock,conf
-
+    global assemble, exe, pipe, deploy_list, build_list, conf
+    flags=[False,False]
+    config_flag=False
     while True:
-        log.log("current assemble status is {}".format(assemble.status.is_alive()))
-        log.log("current exe status is {}".format(exe.status.is_alive()))
-        log.log("assemble auto restart status is {}".format(status_flag[0]))
-        log.log("exe auto restart status is {}".format(status_flag[1]))
+        ans = readinput(10)
+        if ans:
+            if ans == "00":
+                log.log("start config")
+                conf.start(build_list, deploy_list)
+                config_flag = True
+                log.log("config pid is {}".format(conf.status.pid))
+            if ans == "07":
+                log.log("restart all")
+                if flags[0]:
+                    assemble.stop()
+                if flags[1]:
+                    exe.stop()
+                conf.start(build_list, deploy_list)
+                config_flag=True
+                assemble.start()
+                exe.start()
+                log.log("config pid is {}".format(conf.status.pid))
+                log.log("assemble pid is {}".format(assemble.status.pid))
+                log.log("execute pid is {}".format(exe.status.pid))
+            elif config_flag:
+                if ans == "01":
+                    log.log("start assembly")
+                    assemble.start()
+                    flags[0] = True
+                    log.log("assemble pid is {}".format(assemble.status.pid))
+                elif ans == "02":
+                    log.log("start execute")
+                    exe.start()
+                    flags[1] = True
+                    log.log("execute pid is {}".format(exe.status.pid))
+                elif ans == "03":
+                    if flags[0]:
+                        log.log("stop assembly")
+                        assemble.stop()
+                    else:
+                        log.log("assemble process not run and can not stop")
+                elif ans == "04":
+                    if flags[1]:
+                        log.log("stop execute")
+                        exe.stop()
+                    else:
+                        log.log("execute process not run and can not stop")
+
+                elif ans == "05":
+                    if flags[0]:
+                        assemble.stop()
+                    log.log("restart assembly")
+                    assemble.start()
+                    log.log("assemble pid is {}".format(assemble.status.pid))
+                elif ans == "06":
+                    if flags[1]:
+                        exe.stop()
+                    log.log("restart execute")
+                    exe.start()
+                    log.log("execute pid is {}".format(exe.status.pid))
+                elif ans == "08":
+                    if flags[1]:
+                        exe.stop()
+                    if flags[0]:
+                        assemble.stop()
+                    log.log("stop all")
+            else:
+                log.log("config has not been run")
+
         log.log("please select operation")
-        log.log("01:stop assembly")
-        log.log("02:stop execute")
-        log.log("03:start assembly")
-        log.log("04:start execute")
+        log.log("00:start config")
+        log.log("01:start assembly")
+        log.log("02:start execute")
+        log.log("03:stop assembly")
+        log.log("04:stop execute")
         log.log("05:restart assembly")
         log.log("06:restart execute")
         log.log("07:restart all")
-        ans = readinput(10)
-        if ans:
-            if ans == "01":
-                log.log("stop assembly")
-                if assemble.status.is_alive():
-                    assemble.status.terminate()
-                else:
-                    log.log("assembly already stop ,no need again")
-                status_flag[0] = False
-            elif ans == "02":
-                log.log("stop execute")
-                if exe.status.is_alive():
-                    exe.status.terminate()
-                else:
-                    log.log("exe already stop ,no need again")
-                status_flag[1] = False
-            elif ans == "03":
-                if not assemble.status.is_alive():
-                    log.log("start assembly")
-                    assemble = assemble_engine.AssembleEngine(pipe[0], build_list)
-                    assemble.start()
-                else:
-                    log.log("assembly  already running ,no need again")
-                status_flag[0] = True
-            elif ans == "04":
-                if not exe.status.is_alive():
-                    log.log("start execute")
-                    exe = execution_engine.ExecutionEngine(deploy_list, pipe[1])
-                    exe.start()
-                else:
-                    log.log("execute already running ,no need again")
-                status_flag[1] = True
-            elif ans == "05":
-                log.log("restart assembly")
-                assemble.status.terminate()
-                assemble = assemble_engine.AssembleEngine(pipe[0], build_list)
-                assemble.start()
-                status_flag[0] = True
-            elif ans == "06":
-                log.log("restart execute")
-                exe.status.terminate()
-                exe = execution_engine.ExecutionEngine(deploy_list, pipe[1])
-                exe.start()
-                status_flag[1] = True
-            elif ans == "07":
-                log.log("restart all")
-                assemble.status.terminate()
-                exe.status.terminate()
-                conf = configuration_engine.ConfigurationEngine()
-                conf.start(build_list, deploy_list)
-                assemble = assemble_engine.AssembleEngine(pipe[0], build_list)
-                assemble.start()
-                exe = execution_engine.ExecutionEngine(deploy_list, pipe[1])
-                exe.start()
-            else:
-                log.log("unknown input,please retry")
+        log.log("08:stop all")
 
 
 if __name__ == '__main__':
@@ -109,50 +116,50 @@ if __name__ == '__main__':
     print('=================Begin to start controller===================')
     build_list = []
     deploy_list = []
-    log.log('start configuration engine')
     conf = configuration_engine.ConfigurationEngine()
-    conf.start(build_list, deploy_list)
-    log.log("configurator  finished")
-    print('==============start assemble engine======================')
-    log.log('start assemble engine')
     assemble = assemble_engine.AssembleEngine(pipe[0], build_list)
-    assemble.start()
-    log.log('assemble finished')
-    print('=================start execution engine=====================')
-    log.log('start execution engine')
     exe = execution_engine.ExecutionEngine(deploy_list, pipe[1])
-    exe.start()
-    log.log('execution finished')
+    log.log("++++++++++++++++++++++++ Select mode+++++++++++++++++++++++++++++++++")
+    log.log("+++++++++++++++++++++++ 01:manual +++++++++++++++++++++++")
+    log.log("+++++++++++++++++++++++ 02:auto,default +++++++++++++++++++++++")
+    choice = readinput(10)
 
-    time.sleep(5)
-    """restart flag for asssembly and execution ,default is True"""
-    status_flag = [True, True]
+    if choice == "01":
+        watch_thread = threading.Thread(target=operation)
+        watch_thread.start()
+    else:
+        log.log('start configuration engine')
+        conf.start(build_list, deploy_list)
+        log.log("configurator  finished")
+        print('==============start assemble engine======================')
+        log.log('start assemble engine')
+        assemble.start()
+        log.log('assemble finished')
+        print('=================start execution engine=====================')
+        log.log('start execution engine')
+        exe.start()
+        log.log('execution finished')
 
-    """lock for operate engine"""
-    lock = True
-    watch_thread = threading.Thread(target=operation, args=())
-    watch_thread.start()
-
-    while True:
-        time.sleep(5)
-        if status_flag[0]:
+        while True:
+            time.sleep(5)
             log.log("assembly engine pid {} current status is {}".format(assemble.status.pid,
                                                                          str(assemble.status.is_alive())))
             if not assemble.status.is_alive():
-                assemble = assemble_engine.AssembleEngine(pipe[0], build_list)
                 assemble.start()
                 if assemble.status.is_alive():
                     log.log("restart assembly success")
                 else:
                     log.log("restart assembly fail")
 
-        if status_flag[1]:
             log.log("execution engine pid {} current status is {}".format(exe.status.pid,
                                                                           str(exe.status.is_alive())))
             if not exe.status.is_alive():
-                exe = execution_engine.ExecutionEngine(deploy_list, pipe[1])
                 exe.start()
                 if exe.status.is_alive():
                     log.log("restart execution success")
                 else:
                     log.log("restart execution fail")
+
+
+
+
