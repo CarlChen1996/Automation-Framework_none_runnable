a0cc9d6dac036c0c703e2cc86e017ddc0d89a8f2
chencar <carl.chen@hp.com>
2019-06-24 11:14:50 +0800

    update report

--- Framework_Kernel/report.py
+++ Framework_Kernel/report.py
@@ -24,7 +24,7 @@ class Report:
         self.template = template
         self.uut_list = uut_list
         self.data = self.final_data()
-
+        self.data_2 = self.final_data_2()
     def generate(self):
         # print('generate html finished')
         # print(self.script_list)
@@ -39,6 +39,7 @@ class Report:
         }
 
         report_data = self.data['final_data']
+        report_data_2 = self.data_2['final_data_2']
         passCount = self.data['passCount']
         failCount = self.data['failCount']
         norunCount = self.data['norunCount']
@@ -73,6 +74,7 @@ class Report:
         template = env.get_template('tmp.html')
         html = template.render(information=information,
                                final_data=report_data,
+                               final_data_2=report_data_2,
                                data=data,
                                total=total,
                                task_name=self.name,
@@ -104,6 +106,7 @@ class Report:
             # project, case[], pass, fail, norun, total
             final_data.append([uut_name, [], 0, 0, 0, 0])
         # print(test_uut_list)
+
         for v in a:
             # print(v)
             for k in final_data:
@@ -126,10 +129,57 @@ class Report:
         data_dict['failCount'] = failed_case_number
         data_dict['norunCount'] = norun_case_number
         data_dict['count'] = total_case_number
-        print('-------------------------????????????---')
-        print(data_dict)
-        print('-------------------------????????????---')
+        # print('-------------------------????????????---')
+        # print(data_dict)
+        # print('-------------------------????????????---')
         return data_dict
+
+    def final_data_2(self):
+        file = os.path.join(os.getcwd(), 'Report\\{}\\result.yaml'.format(self.name))
+        passed_case_number = 0
+        failed_case_number = 0
+        norun_case_number = 0
+        data_dict_2 = {}
+        test_case_list = []
+        final_data_2 = []
+        f = open(file, encoding='utf-8')
+        a = yaml.safe_load(f.read())
+
+        for v in a:
+            if v['case_name'] not in test_case_list:
+                test_case_list.append(v['case_name'])
+
+        for case_name in test_case_list:
+            # project, case[], pass, fail, norun, total
+            final_data_2.append([case_name, [], 0, 0, 0, 0])
+        # print(test_uut_list)
+
+        for v in a:
+            # print(v)
+            for k in final_data_2:
+                if v['case_name'] == k[0]:
+                    index = final_data_2.index(k)
+                    final_data_2[index][1].append(v)
+                    if v['result'] == 'Pass':
+                        final_data_2[index][2] += 1
+                        passed_case_number += 1
+                    if v['result'] == 'Fail':
+                        final_data_2[index][3] += 1
+                        failed_case_number += 1
+                    if v['result'] == 'Norun':
+                        final_data_2[index][4] += 1
+                        norun_case_number += 1
+                    final_data_2[index][5] += 1
+        total_case_number = passed_case_number + failed_case_number + norun_case_number
+        data_dict_2['final_data_2'] = final_data_2
+        data_dict_2['passCount'] = passed_case_number
+        data_dict_2['failCount'] = failed_case_number
+        data_dict_2['norunCount'] = norun_case_number
+        data_dict_2['count'] = total_case_number
+        # print('-------------------------????????????---')
+        # print(data_dict_2)
+        # print('-------------------------????????????---')
+        return data_dict_2
 
     def result(self):
         result = []
