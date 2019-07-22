3cdac4bd9b9350f02688b82edf23373061395430
chengba <balance.cheng@hp.com>
2019-07-22 11:14:22 +0800

    Update Deploy and Execute Function

--- Framework_Kernel/QTPutils.py
+++ Framework_Kernel/QTPutils.py
@@ -9,55 +9,73 @@ import os
 import re
 import ftplib
 import openpyxl
+from Framework_Kernel.task import Task
+from Framework_Kernel.host import WindowsExecuteHost
 
 
 class QTP_HPDM:
     def __init__(self):
         self.__ftp = r'15.83.248.251'
-        self.__ftp_user = r'automation'
-        self.__ftp_passwd = 'Shanghai2010'
         self.__ip = "15.83.248.251"
-        self.__create_filter_path = r'c:\Scripts\CreateFilter'
-        self.__create_template_path = r'c:\Scripts\CreateTemplates'
-        self.__send_command_path = r'c:\Scripts\SendCommand'
-        self.__send_packages_path = r'c:\Scripts\SendPackages'
-        self.__get_result_path = r'c:\Scripts\CaptureFiles'
-
-    def get_mac_by_ip(self, ip):
-        pattern_mac = re.compile('([a-f0-9]{2}[-:]){5}[a-f0-9]{2}', re.I)
-        os.popen('ping -n 1 -w 500 {} > nul'.format(ip))
-        macaddr = os.popen('arp -a {}'.format(ip))
-        macaddr = pattern_mac.search(macaddr.read())
-        if macaddr:
-            return macaddr.group()
-        else:
-            return None
+        self.__create_filter_path = r'c:\ScriptsCopy\CreateFilter'
+        self.__create_template_path = r'c:\ScriptsCopy\CreateTemplates'
+        self.__send_command_path = r'c:\ScriptsCopy\SendCommand'
+        self.__send_packages_path = r'c:\ScriptsCopy\SendPackages'
+        self.__get_result_path = r'c:\ScriptsCopy\CaptureFiles'
+        self.__discover_devices_path = r'c:\ScriptsCopy\DiscoverDevices'
+        root = os.path.dirname(os.path.dirname(__file__))
+        self.__test_data_path = os.path.join(root, 'Configuration\\test_data.xlsx')
 
     def set_test_data(self, task):
-        # ftp = ftplib.FTP(self.__ftp)
-        root = os.path.dirname(os.path.dirname(__file__))
-        test_data_path = os.path.join(root, 'Configuration\\test_data.xlsx')
-        workbook = openpyxl.load_workbook(test_data_path)
-        sheet_uut = workbook['UUT']
+        workbook = openpyxl.load_workbook(self.__test_data_path)
         """
         col1:IP
         col2:Mac
-        col3:OS
         """
-        rows = sheet_uut.max_row
-        for i in range(2, rows+1):
-            """
-            Delete all the data in sheet
-            """
-            ip = sheet_uut.cell(i, 1).value
-            mac = sheet_uut.cell(i, 2).value
-            _os = sheet_uut.cell(i, 3).value
-            print(ip, mac, _os)
-            sheet_uut.delete_rows(2)
-            workbook.save(test_data_path)
+        # ------Delete exist Data-------------------------------------------
+        os_list = {"ThinPro7":"HP ThinPro 7", "WES7P":"WES7P-64", "WES10":"Win10IoT-64", "WES7E":"WES7E"}
+        # print(list(os_list.values()))
+        for os_item in os_list.values():
+            sheet_uut = workbook['UUT_{}'.format(os_item)]
+            rows = sheet_uut.max_row
+            for i in range(2, rows+1):
+                """
+                Delete all the data in sheet
+                """
+                sheet_uut.delete_rows(2)
+                workbook.save(self.__test_data_path)
+        # ---------------------------------------------------------------------
+        # ----------Set UUT Data according task uut_list---------------------------------
+        for uut in task.get_uut_list():
+            if uut.get_version() in os_list.keys():
+                sheet_uut = workbook['UUT_{}'.format(os_list[uut.get_version()])]
+                max_row = sheet_uut.max_row
+                print(uut.get_version(),sheet_uut.max_row)
+                sheet_uut.cell(max_row+1, 1).value=uut.get_ip()
+                sheet_uut.cell(max_row+1, 2).value=uut.get_mac()
+                workbook.save(self.__test_data_path)
+        # ---------Set Config Data according task uut_list exe_list ------------------
+        package_path = os.path.dirname(task.get_exe_file_list()[0])
+        sheet_config = workbook['Config']
+        if 'LINUX' in package_path.upper():
+            sheet_config.cell(2, 1).value = "Linux_{}".format(task.get_name())
+        elif 'WINDOW' in package_path.upper():
+            sheet_config.cell(2, 1).value = "Windows_{}".format(task.get_name())
+        else:
+            sheet_config.cell(2, 1).value = "Unknown_{}".format(task.get_name())
+        sheet_config.cell(2, 2).value = 'c:\\inetpub\\ftproot\\{}'.format(package_path)
+        workbook.save(self.__test_data_path)
+        self.__upload_test_data()
+
+    def __upload_test_data(self):
+        ftp = ftplib.FTP(self.__ftp)
+        ftp.login('automation','Shanghai2010')
+        print(ftp.nlst())
+        ftp.storbinary('STOR test_data.xlsx', open(self.__test_data_path, 'rb'), 1024)
+        ftp.close()
 
     def __run_qtp_script(self, testPath):
-        qtp = win32com.client.DispatchEx("QuickTest.Application", self.ip)
+        qtp = win32com.client.DispatchEx("QuickTest.Application", self.__ip)
         qtp.Launch()
         qtp.Visible = True
         qtp.Open(testPath)
@@ -100,10 +118,10 @@ class Deploy:
         pass
 
     def deploy(self, task):
-        QTP_HPDM().deploy(task)
+        QTP_HPDM().deploy_task(task)
 
 
-class Execute(QTP_HPDM):
+class Execute():
     def __init__(self):
         pass
 
@@ -117,5 +135,17 @@ class Execute(QTP_HPDM):
         QTP_HPDM().get_result(task)
 
 
-QTP_HPDM().set_test_data('test')
-# print(__file__)
+if __name__ == '__main__':
+
+    task = Task('precheck')
+    uut1 = WindowsExecuteHost('15.83.248.208', '7C:D3:0A:05:01:97', '', 'WES10', 'Admin', 'Admin', '', 'on')
+    uut2 = WindowsExecuteHost('15.83.250.20', '48:0F:CF:BC:DD:3C', '', 'WES10', 'Admin', 'Admin', '', 'on')
+    uut3 = WindowsExecuteHost('15.83.250.205', '48:0F:CF:BB:7C:65', '', 'WES7E', 'Administrator', 'Administrator', '', 'on')
+    uut4 = WindowsExecuteHost('15.83.250.210', '48:0F:CF:BB:7F:65', '', 'WES7E', 'Administrator', 'Administrator', '', 'on')
+    task.insert_uut_list(uut1)
+    task.insert_uut_list(uut2)
+    task.insert_uut_list(uut3)
+    task.insert_uut_list(uut4)
+    task.insert_exe_file_list(r'C:\inetpub\ftproot\jenkins\windows\task_2\run.exe')
+    # QTP_HPDM().set_test_data(task)
+    QTP_HPDM().set_test_data(task)
