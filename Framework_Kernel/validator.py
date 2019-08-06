f3e845ccb62cfa99df2527ff6630b1529418b6e4
chencar <carl.chen@hp.com>
2019-08-06 16:26:49 +0800

    add validate function but did not change reference yet

--- Framework_Kernel/validator.py
+++ Framework_Kernel/validator.py
@@ -4,6 +4,9 @@
 # @Email   : carl.chen@hp.com
 # @File    : Validator.py
 # @Project : Automation-Framework
+import shlex
+import subprocess
+from Framework_Kernel.log import configuration_log,assemble_log
 
 
 class Validator:
@@ -13,9 +16,48 @@ class Validator:
 
 class HostValidator(Validator):
     def validate(self, host):
-        print('validate ' + host.get_hostname() + ' finished')
+        print('validate ' + host.get_ip() + ' pass')
         # controller_log.info('validate ' + host.get_hostname() + ' finished')
         return True
+
+    @staticmethod
+    def validate_build_server(host):
+        result = ping(host.get_ip())
+        if result:
+            configuration_log.info('validate ' + host.get_ip() + ' pass')
+            host.status = 'on'
+            return True
+        else:
+            configuration_log.info('validate ' + host.get_ip() + ' fail')
+            host.status = 'off'
+            return False
+
+    @staticmethod
+    def validate_deploy_server(host):
+        result = ping(host.get_ip())
+        if result:
+            configuration_log.info('validate ' + host.get_ip() + ' pass')
+            host.status = 'on'
+            return True
+        else:
+            configuration_log.info('validate ' + host.get_ip() + ' fail')
+            host.status = 'off'
+            return False
+
+    @staticmethod
+    def validate_uut(host):
+        result = ping(host.get_ip())
+        if result:
+            assemble_log.info('validate ' + host.get_ip() + ' pass')
+            host.status = 'on'
+            return True
+        else:
+            assemble_log.info('validate ' + host.get_ip() + ' fail')
+            host.status = 'off'
+            return False
+
+    def validate_http(self):
+        pass
 
 
 class ScriptValidator(Validator):
@@ -23,3 +65,22 @@ class ScriptValidator(Validator):
         print('validate ' + task.get_name() + ' scripts finished')
         # controller_log.info('validate ' + task.get_name() + ' scripts finished')
         return True
+
+
+def ping(ip):
+    cmd = "ping -n 1 {}".format(ip)
+    args = shlex.split(cmd)
+    try:
+        subprocess.check_call(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        return True
+
+    except subprocess.CalledProcessError:
+        return False
+
+
+if __name__ == '__main__':
+    from Framework_Kernel.host import WindowsExecuteHost
+    h=WindowsExecuteHost(ip='15.83.248.251',mac='12121212212')
+    a=HostValidator.validate_uut(h)
+    print(a)
+
