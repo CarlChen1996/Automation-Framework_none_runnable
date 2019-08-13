4005703b8d8868ecd09659abfc6437be9c4a7cf8
kit <jie.liu1@hp.com>
2019-08-13 15:02:00 +0800

    update unittest code: test_log.py, test_validator.py;
    update README file of Framework_unittest;
    
    modify unittest excel

--- Framework_unittest/test_log.py
+++ Framework_unittest/test_log.py
@@ -9,17 +9,50 @@ from unittest.mock import patch
 import unittest
 import os
 
+'''
+read_log: read last log message
+get_log_file: get the newest log file
+get_newest_file: get get the newest file from directory
+test_lower_level_log: lower level log message can not be log
+test_equal_level_log: equal level log message can be log
+test_higher_level_log: lower level log message can be log
+test_custom_level: log with custom level
+test_custom_separator: log with custom separator
+test_log_critical: critical method can be called
+test_log_error: error method can be called
+test_log_warning: warning method can be called
+test_log_info: info method can be called
+test_log_debug: debug method can be called
+test_custom_console_False: test without user console
+test_custom_console_True: test with user console
+'''
 
-class AnalyzerTest(unittest.TestCase):
+
+log_folder_name = 'unittest'
+unittest_log = log.Log(name=log_folder_name, level='INFO')
+custom_log_name = 'custom'
+custom_separator = '?'
+custom_log = log.Log(name=custom_log_name, level='ERROR', separator=custom_separator)
+
+
+class LogTest(unittest.TestCase):
     def read_log(self, log_name):
         file = self.get_log_file(log_name)
-        with open(file, 'w') as f:
-            return f.readlines()[-1]
+        with open(file, 'r') as f:
+            try:
+                log_msg = f.readlines()[-1]
+            except Exception as e:
+                if type(e) == IndexError:
+                    log_msg = ''
+                else:
+                    print(e)
+                    return False
+            return log_msg
 
     def get_log_file(self, log_name):
         folder = self.get_newest_file('.\\Log')
         log_folder = os.path.join(folder, log_name)
-        return os.path.join(log_folder, self.get_newest_file(log_folder))
+        return self.get_newest_file(log_folder)
 
     @staticmethod
     def get_newest_file(log_path):
@@ -27,57 +60,87 @@ class AnalyzerTest(unittest.TestCase):
         lists.sort(key=lambda fn: os.path.getmtime(log_path + '\\' + fn))
         return os.path.join(log_path, lists[-1])
 
-    def setUp(self):
-        self.log_name = 'unittest'
-        self.log = log.Log(name=self.log_name)
+    def test_lower_level_log(self):
+        unittest_log.debug('lower level log')
+        log_msg = self.read_log(log_folder_name)
+        if log_msg is False:
+            log_msg = ''
+            print('read lower level log raise exception')
+        self.assertNotIn('lower level log', log_msg)
+
+    def test_equal_level_log(self):
+        unittest_log.info('equal level log')
+        log_msg = self.read_log(log_folder_name)
+        if log_msg is False:
+            log_msg = ''
+            print('read equal level log raise exception')
+        self.assertIn('equal level log', log_msg)
+
+    def test_higher_level_log(self):
+        unittest_log.warning('higher level log')
+        log_msg = self.read_log(log_folder_name)
+        if log_msg is False:
+            log_msg = ''
+            print('read higher level log raise exception')
+        self.assertIn('higher level log', log_msg)
+
+    def test_custom_level(self):
+        custom_log.info('custom level log')
+        log_msg = self.read_log(custom_log_name)
+        if log_msg is False:
+            log_msg = ''
+            print('read custom level log raise exception')
+        self.assertNotIn('custom level log', log_msg)
+        custom_log.error('write log with custom level')
+        log_msg = self.read_log(custom_log_name)
+        if log_msg is False:
+            log_msg = ''
+            print('read custom level log raise exception')
+        self.assertIn('write log with custom level', log_msg)
+
+    def test_custom_separator(self):
+        custom_log.error('write log with custom separator')
+        log_msg = self.read_log(custom_log_name)
+        if log_msg is False:
+            log_msg = ''
+            print('read custom separator log raise exception')
+        self.assertIn(custom_separator, log_msg)
 
     @patch('logging.Logger.critical')
     def test_log_critical(self, critical_mock):
-        self.log.critical('test')
+        unittest_log.critical('test')
         critical_mock.assert_called_once_with('test')
-
-    @patch('logging.Logger.warning')
-    def test_log_warning(self, warning_mock):
-        self.log.warning('test')
-        warning_mock.assert_called_once_with('test')
 
     @patch('logging.Logger.error')
     def test_log_error(self, error_mock):
-        self.log.error('test')
+        unittest_log.error('test')
         error_mock.assert_called_once_with('test')
+
+    @patch('logging.Logger.warning')
+    def test_log_warning(self, warning_mock):
+        unittest_log.warning('test')
+        warning_mock.assert_called_once_with('test')
 
     @patch('logging.Logger.info')
     def test_log_info(self, info_mock):
-        self.log.info('test')
+        unittest_log.info('test')
         info_mock.assert_called_once_with('test')
 
     @patch('logging.Logger.debug')
     def test_log_debug(self, debug_mock):
-        self.log.debug('test')
+        unittest_log.debug('test')
         debug_mock.assert_called_once_with('test')
 
-    def test_write_high_level_log(self):
-        pass
-
-    def test_write_low_level_log(self):
-        pass
-
     @patch('logging.Formatter')
-    def test_custom_separator_False(self, separator_mock):
-        separator = ':'
+    def test_custom_console_False(self, separator_mock):
         user_console = False
-        log.Log(name=self.log_name, level='INFO', separator=separator, use_console=user_console)
+        log.Log(name='user_console', level='INFO', separator=custom_separator, use_console=user_console)
         separator_mock.assert_called_once_with("[%(asctime)s] {} %(name)s {} [%(levelname)s] {} %(message)s"
-                                               .format(separator, separator, separator))
+                                               .format(custom_separator, custom_separator, custom_separator))
         self.assertEqual(separator_mock.call_count, 1)
 
     @patch('logging.Formatter')
-    def test_custom_separator_True(self, separator_mock):
-        separator = ':'
+    def test_custom_console_True(self, separator_mock):
         user_console = True
-        log.Log(name=self.log_name, level='INFO', separator=separator, use_console=user_console)
+        log.Log(name='user_console', level='INFO', separator=custom_separator, use_console=user_console)
         self.assertEqual(separator_mock.call_count, 2)
-
-
-if __name__ == '__main__':
-    unittest.main()
