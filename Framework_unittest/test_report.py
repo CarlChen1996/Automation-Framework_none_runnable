8e2b6890cb14c0fe1240a83ee77f6052f1663b5f
kit <jie.liu1@hp.com>
2019-08-21 10:16:19 +0800

    update unittest code for test_report.py

--- Framework_unittest/test_report.py
+++ Framework_unittest/test_report.py
@@ -4,7 +4,7 @@
 # @Email   : jie.liu1@hp.com
 # @File    : test_report.py
 # @Project : Automation-Framework
-from Framework_Kernel import report, host
+from Framework_Kernel import report, host, task
 import os
 import unittest
 import shutil
@@ -24,15 +24,17 @@ class ReportTest(unittest.TestCase):
             shutil.rmtree(self.static_folder)
         if os.path.exists(self.html):
             os.remove(self.html)
-        host1 = host.WindowsExecuteHost("15.83.248.208", "")
-        host2 = host.WindowsExecuteHost("15.83.250.20", "")
-        self.host_list = [host1, host2]
+        host1 = host.WindowsExecuteHost('15.83.248.208', '')
+        host2 = host.WindowsExecuteHost('15.83.250.20', '')
+        self.task = task.Task(name='report_for_unittest')
+        self.task.insert_uut_list(host1)
+        self.task.insert_uut_list(host2)
 
     def tearDown(self):
         shutil.rmtree(self.static_folder)
         os.remove(self.html)
 
     def test_generate_report(self):
-        test_report = report.Report("report_for_unittest", self.host_list)
+        test_report = report.Report(self.task)
         test_report.generate()
-        self.assertIn("report_for_unittest.html", os.listdir(".\\Report\\report_for_unittest"))
+        self.assertIn('report_for_unittest.html', os.listdir('.\\Report\\report_for_unittest'))
