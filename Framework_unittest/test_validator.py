4450dc339b7d734ee0b686f91f84e7c264d34687
kit <jie.liu1@hp.com>
2019-09-10 14:26:07 +0800

    update unittest code and README file;

--- Framework_unittest/test_validator.py
+++ Framework_unittest/test_validator.py
@@ -10,13 +10,17 @@ import unittest
 
 '''
 setUp: Initialize variables
+test_ping_true: ping to available ip
+test_ping_false: ping to unavailable ip
 test_build_server: check connectivity of build server
 test_unavailable_build_server: check return value with unavailable build ip
 test_deploy_server: check connectivity of deploy server
-test_unavailable_deploy_server: check return value with unavailable deploy ip
+test_unavailable_deploy_server_qtp_false: check return value with unavailable qtp server
+test_unavailable_deploy_server_hpdm_false: check return value with unavailable hpdm server
 test_UUT: check connectivity of UUT
 test_unavailable_UUT: check return value with unavailable uut ip
 test_ftp: check connectivity of ftp server
+test_unavailable_ftp
 '''
 
 
@@ -36,22 +40,53 @@ class ValidatorTest(unittest.TestCase):
                                               hostname='unavailable_uut_host')
         self.host_validator = validator.HostValidator()
 
-    def test_build_server(self):
-        self.assertTrue(self.host_validator.validate_build_server(self.build_server))
+    def test_ping_true(self):
+        self.assertTrue(validator.Validator().ping(self.ip))
 
-    def test_unavailable_build_server(self):
+    def test_ping_false(self):
+        self.assertFalse(validator.Validator().ping(self.unavailable_ip))
+
+    @patch('Framework_Kernel.validator.HostValidator.get_jenkins_node_state')
+    @patch('Framework_Kernel.validator.Validator.ping')
+    def test_build_server(self, ping_mock, jenkins_node_mock):
+        ping_mock.return_value = True
+        self.assertTrue(self.host_validator.validate_build_server(self.build_server))
+        jenkins_node_mock.assert_called_once_with(self.build_server)
+
+    @patch('Framework_Kernel.validator.Validator.ping')
+    def test_unavailable_build_server(self, ping_mock):
+        ping_mock.return_value = False
         self.assertFalse(self.host_validator.validate_build_server(self.unavailable_build_server))
 
-    def test_deploy_server(self):
+    @patch('Framework_Kernel.validator.HostValidator._HostValidator__validate_QTP')
+    @patch('Framework_Kernel.validator.HostValidator._HostValidator__validate_HPDM')
+    def test_deploy_server(self, qtp_mock, hpdm_mock):
+        qtp_mock.return_value = True
+        hpdm_mock.return_value = True
         self.assertTrue(self.host_validator.validate_deploy_server(self.deploy_server))
 
-    def test_unavailable_deploy_server(self):
+    @patch('Framework_Kernel.validator.HostValidator._HostValidator__validate_HPDM')
+    @patch('Framework_Kernel.validator.HostValidator._HostValidator__validate_QTP')
+    def test_unavailable_deploy_server_qtp_false(self, qtp_mock, hpdm_mock):
+        qtp_mock.return_value = False
+        self.assertFalse(self.host_validator.validate_deploy_server(self.unavailable_deploy_server))
+        self.assertFalse(hpdm_mock.called)
+
+    @patch('Framework_Kernel.validator.HostValidator._HostValidator__validate_HPDM')
+    @patch('Framework_Kernel.validator.HostValidator._HostValidator__validate_QTP')
+    def test_unavailable_deploy_server_hpdm_false(self, qtp_mock, hpdm_mock):
+        qtp_mock.return_value = True
+        hpdm_mock.return_value = False
         self.assertFalse(self.host_validator.validate_deploy_server(self.unavailable_deploy_server))
 
-    def test_UUT(self):
+    @patch('Framework_Kernel.validator.Validator.ping')
+    def test_UUT(self, ping_mock):
+        ping_mock.return_value = True
         self.assertTrue(self.host_validator.validate_uut(self.uut_host))
 
-    def test_unavailable_UUT(self):
+    @patch('Framework_Kernel.validator.Validator.ping')
+    def test_unavailable_UUT(self, ping_mock):
+        ping_mock.return_value = False
         self.assertFalse(self.host_validator.validate_uut(self.unavailable_uut_host))
 
     def test_ftp(self):
