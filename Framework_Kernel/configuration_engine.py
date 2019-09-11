42c67ef12ae97c8919e6d3712567b19a02f19d6d
sich <chen.si@hp.com>
2019-09-11 16:50:56 +0800

    Refact init_server function, use dict and pass functions instead of using if, else block

--- Framework_Kernel/configuration_engine.py
+++ Framework_Kernel/configuration_engine.py
@@ -5,7 +5,7 @@
 # @File    : test.py
 # @Project : demo
 from Framework_Kernel.engine import Engine
-from Framework_Kernel.host import WindowsBuildHost, WindowsDeployHost,LinuxBuildHost
+from Framework_Kernel.host import WindowsBuildHost, WindowsDeployHost, LinuxBuildHost, LinuxDeployHost
 from Framework_Kernel.analyzer import Analyzer
 from Framework_Kernel.validator import HostValidator
 from Framework_Kernel.log import configuration_log
@@ -21,70 +21,61 @@ class ConfigurationEngine(Engine):
         self.config_server_list = os.path.join(
             (os.path.abspath(r".\Configuration")), "config_server_list.yml")
         self.list_status = False
+        self.server_map_dict = {
+            ('windows', 'build'): WindowsBuildHost,
+            ('windows', 'deploy'): WindowsDeployHost,
+            ('linux', 'build'): LinuxBuildHost,
+            ('linux', 'deploy'): LinuxDeployHost
+        }
 
     def start(self):
         self.build_server_list.clear()
         self.deploy_server_list.clear()
-        self.__configuration_process = Process(target=self.run,
-                                               args=())
+        self.__configuration_process = Process(target=self.run, args=())
         self.__configuration_process.start()
         self.status = self.__configuration_process
         receive_signal = self.receive_signal.recv()
         for valid_server in receive_signal:
             if isinstance(valid_server, WindowsBuildHost):
                 self.build_server_list.append(valid_server)
-                configuration_log.info("Add Windows Build Host {}".format(valid_server.get_hostname()))
+                configuration_log.info("Add Windows Build Host {}".format(
+                    valid_server.get_hostname()))
             elif isinstance(valid_server, WindowsDeployHost):
                 self.deploy_server_list.append(valid_server)
-                configuration_log.info("Add Windows Deploy Host {}".format(valid_server.get_hostname()))
-            elif isinstance(valid_server,LinuxBuildHost):
+                configuration_log.info("Add Windows Deploy Host {}".format(
+                    valid_server.get_hostname()))
+            elif isinstance(valid_server, LinuxBuildHost):
                 self.build_server_list.append(valid_server)
-                configuration_log.info("Add linux Build Host {}".format(valid_server.get_hostname()))
+                configuration_log.info("Add linux Build Host {}".format(
+                    valid_server.get_hostname()))
+            elif isinstance(valid_server, LinuxDeployHost):
+                self.deploy_server_list.append(valid_server)
+                configuration_log.info("Add linux Deploy Host {}".format(
+                    valid_server.get_hostname()))
         self.list_status = True
         return self.list_status
 
     def get_server_list(self):
         server_list_analyzer = Analyzer()
-        server_list = server_list_analyzer.analyze_file(self.config_server_list)
+        server_list = server_list_analyzer.analyze_file(
+            self.config_server_list)
         return server_list
 
-    def __initial_server(self, server_item):
-        if server_item['os'] == 'windows' and server_item[
-                'function'] == 'deploy':
-            server = WindowsDeployHost(ip=server_item['ip'],
-                                       hostname=server_item['hostname'],
-                                       version=server_item['version'],
-                                       mac=server_item['mac'],
-                                       username=server_item['username'],
-                                       password=server_item['password'],
-                                       domain=server_item['domain'])
+    def __init_server(self, server_item):
+        current_key = (server_item['os'], server_item['function'])
+        if current_key in self.server_map_dict.keys():
+            server = self.server_map_dict[current_key](
+                ip=server_item['ip'],
+                hostname=server_item['hostname'],
+                version=server_item['version'],
+                mac=server_item['mac'],
+                username=server_item['username'],
+                password=server_item['password'],
+                domain=server_item['domain'])
             configuration_log.info('Init {}'.format(server_item['hostname']))
-        elif server_item['os'] == 'windows' and server_item[
-                'function'] == 'build':
-            server = WindowsBuildHost(ip=server_item['ip'],
-                                      hostname=server_item['hostname'],
-                                      version=server_item['version'],
-                                      mac=server_item['mac'],
-                                      username=server_item['username'],
-                                      password=server_item['password'],
-                                      domain=server_item['domain'])
-            configuration_log.info('Init {}'.format(server_item['hostname']))
-        elif server_item['os'] == 'linux' and server_item[
-                'function'] == 'deploy':
-            # TODO Add LinuxDeployHost
-            server = True
-        elif server_item['os'] == 'linux' and server_item[
-                'function'] == 'build':
-            # TODO Add LinuxBuildHost
-            # server = True
-            server = LinuxBuildHost(ip=server_item['ip'],
-                                      hostname=server_item['hostname'],
-                                      version=server_item['version'],
-                                      mac=server_item['mac'],
-                                      username=server_item['username'],
-                                      password=server_item['password'],
-                                      domain=server_item['domain'])
-            configuration_log.info('Init {}'.format(server_item['hostname']))
+        else:
+            print('Invalid server type {}'.format(current_key))
+            server = False
         return server
 
     def validate_server(self, server):
@@ -105,7 +96,7 @@ class ConfigurationEngine(Engine):
         server_list_result = self.get_server_list()
         valid_server_list = []
         for server_item in server_list_result:
-            server = self.__initial_server(server_item)
+            server = self.__init_server(server_item)
             if self.validate_server(server):
                 valid_server_list.append(server)
         self.send_signal.send(valid_server_list)
